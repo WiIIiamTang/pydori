3e1727ba38d93c7b1797f7ce333b5174c1854f91
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2020-06-04 20:21:39 -0400

    setup typo

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8'
+    'Programming Language :: Python :: 3.8',
     "Operating System :: OS Independent"
   ],
 )
